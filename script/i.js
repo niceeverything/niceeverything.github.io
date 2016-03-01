c6ba9cc14d8ddd5ecd91da89d4f29bac96ebf6ca
niceeverything <brucewayne.kid@qq.com>
2016-03-01 16:14:02 +0800

    sdf

---
+++ script/i.js
@@ -0,0 +1,46 @@
+$(function() {
+     $('.vvbody').width($(window).width()).height($(document).height());
+	 $('.vvbody img').width($(window).width()).height($(document).height());
+	// $('#container').animate({
+	// 	left: 0
+	// }, {
+	// 	duration: 1500,
+	// 	easing: "easeInOutBack"
+	// });
+
+	$('#logo').animate({
+		top: 0
+	}, {
+		duration: 1000
+	});
+
+	$('#nav').animate({
+		top: 200
+	}, {
+		duration: 1000
+	});
+
+	$('#momenu').animate({
+		top: 420
+	}, {
+		duration: 1000
+	});
+	$('#sub').animate({
+		top: 500
+	}, {
+		duration: 1000
+	});
+
+	$('#dlw').animate({
+		top: 620
+	}, {
+		duration: 1000
+	});
+
+	$('#footer').animate({
+		top: 900
+	}, {
+		duration: 1000
+	});
+
+});
